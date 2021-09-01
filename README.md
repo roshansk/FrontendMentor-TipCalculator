1a7d7e2d35a862b1e7f4f3845ad97f749dae18c0
Roshan Kamble <roshansk16@gmail.com>
2021-09-01 21:20:02 +0530

    First Build

---
+++ README.md
@@ -0,0 +1,54 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+## Table of contents
+
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+- Mobile & Tablet View
+  Mobile: 375px             | Tablet: 786px
+  :-------------------------:|:-------------------------:
+  ![](./screenshots/mobile.png)  |  ![](./screenshots/tablet.png)
+
+- Desktop: 1024px
+![](./screenshots/desktop.png)
+
+- Desktop: 1440px
+![](./screenshots/widescreen.png)
+
+### Links
+
+- [Solution URL]()
+- [Live site](https://roshansk.github.io/FrontendMentor-TipCalculator)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla Js
+
+## Author
+
+- Frontend Mentor - [@roshansk](https://www.frontendmentor.io/profile/roshansk)
+
---
+++ splitter.js
@@ -0,0 +1,121 @@
+class Bill{
+    
+    constructor(bill,nop,tip,customTip){
+        this.bill = bill;
+        this.nop = nop;
+        this.tip = tip;
+        this.customTip = customTip;
+    }
+
+    
+    calcualteBill(){
+        console.log(this.bill , this.nop , this.tip , this.customTip);
+        if(this.bill && this.nop && (this.tip || this.customTip)){
+            const tipPerPerson = this.tip ? parseFloat( (this.bill*(this.tip/100)) / this.nop ).toFixed(2) : parseFloat(this.customTip/this.nop).toFixed(2);
+            const billPerPerson = parseFloat( Number((this.bill/this.nop))  + Number(tipPerPerson)).toFixed(2);
+            this.updateBill(billPerPerson,tipPerPerson);
+        }
+    }
+
+    updateBill(bill,tip){
+        document.querySelector('span#tip-amount').innerHTML = tip;
+        document.querySelector('span#bill-amount').innerHTML = bill;
+        document.querySelector(`#reset`).disabled = false; 
+    }
+
+    reset(){
+        this.bill = 0;
+        this.tip = 0;
+        this.nop = 0;
+        this.customTip = 0;
+    }
+}
+
+
+const clearSelectedtip = () => {
+    document.querySelectorAll(`input[type='button'].btn-tip`).forEach( btn => btn.classList.remove('tip-selected') );
+}
+
+
+
+
+
+window.onload = () => {
+    
+    const newBill = new Bill;
+    //Bill input validation
+    document.querySelector(`input[name='bill']`).addEventListener('input', (e) => {
+        
+        const bill = Number(e.target.value);
+        
+        if( e.target.value.length > 0 && bill < 1 ){
+            e.target.parentNode.classList.add('error');
+            e.target.classList.add('error');
+        }
+        else{
+            e.target.classList.remove('error');
+            e.target.parentNode.classList.remove('error');
+            newBill.bill = bill;
+            newBill.calcualteBill();
+        }
+
+    })
+
+      //Tip input validation
+      document.querySelector(`input[name='custom-tip']`).addEventListener('input', (e) => {
+        
+        clearSelectedtip();
+        
+        const tip = Number(e.target.value);
+
+        if( e.target.value.length > 0 && tip < 1 ){
+            e.target.parentNode.classList.add('error');
+            e.target.classList.add('error');
+        }
+        else{
+            e.target.classList.remove('error');
+            e.target.parentNode.classList.remove('error');
+            newBill.customTip = tip;
+            newBill.tip = undefined;
+            newBill.calcualteBill();
+        }
+    })
+
+    //nop - Number of People Validation
+    document.querySelector(`input[name='nop']`).addEventListener('input', (e) => {
+        
+        const nop = Number(e.target.value);
+
+        if( e.target.value.length > 0 &&  nop < 1 ){
+            e.target.parentNode.classList.add('error');
+            e.target.classList.add('error');
+        }
+        else{
+            e.target.classList.remove('error');
+            e.target.parentNode.classList.remove('error');
+            newBill.nop = nop;
+            newBill.calcualteBill();
+        }
+    })
+
+
+    //Tip buttons 
+    document.querySelectorAll(`input[type='button'].btn-tip`).forEach( btn => btn.addEventListener('click', (e) => {
+        clearSelectedtip();
+        document.querySelector(`input[name='custom-tip']`).value="";
+        e.target.classList.add('tip-selected');
+        const tip = Number(e.target.value.substring(0,e.target.value.length - 1));
+        newBill.tip = tip;
+        newBill.customTip = undefined;
+        newBill.calcualteBill();
+    }) )
+
+    //Reset
+    document.querySelector(`#reset`).addEventListener('click', (e) => {
+        document.querySelectorAll(`input[type='number']`).forEach( input => input.value = "" );
+        document.querySelectorAll('.amount').forEach( amount => amount.innerHTML = "0.00" );
+        document.querySelectorAll('.btn-tip').forEach( btn => btn.classList.remove('tip-selected'));
+        newBill.reset();
+    })
+
+}
